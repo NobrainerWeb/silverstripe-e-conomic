49bab91718d3ba455246140b95313d87ce1dbd68
Carsten Meldgaard <carstenmeldgaard@Fonas-iMac.local>
2017-09-12 08:28:17 +0200

    Converted spaces to tabs

--- code/EconomicClient.php
+++ code/EconomicClient.php
@@ -12,180 +12,180 @@ use Lenius\Economic\RestClient;
  */
 class EconomicClient extends \Object
 {
-    /**
-     * @config
-     */
-    protected static $secretToken;
+	/**
+	 * @config
+	 */
+	protected static $secretToken;
 
-    /**
-     * @config
-     */
-    protected static $agreementGrantToken;
+	/**
+	 * @config
+	 */
+	protected static $agreementGrantToken;
 
-    /**
-     * @var string
-     */
-    private static $currency;
+	/**
+	 * @var string
+	 */
+	private static $currency;
 
-    /**
-     * TODO this should be able to be chosen for each invoice
-     * @var int
-     */
-    private static $invoice_layout_number;
+	/**
+	 * TODO this should be able to be chosen for each invoice
+	 * @var int
+	 */
+	private static $invoice_layout_number;
 
-    /**
-     * paymentTermsNumber is required by e-conomic, the other keys are not.
-     * @var array
-     */
-    private static $payment_terms = [
-        'paymentTermsNumber' => null,
-        'daysOfCredit'       => null,
-        'name'               => null,
-        'paymentTermsType'   => null
-    ];
+	/**
+	 * paymentTermsNumber is required by e-conomic, the other keys are not.
+	 * @var array
+	 */
+	private static $payment_terms = [
+		'paymentTermsNumber' => null,
+		'daysOfCredit'       => null,
+		'name'               => null,
+		'paymentTermsType'   => null
+	];
 
-    /**
-     * @object RestClient
-     */
-    protected $client;
+	/**
+	 * @object RestClient
+	 */
+	protected $client;
 
-    /**
-     * JSON data
-     * @var
-     */
-    protected $response;
+	/**
+	 * JSON data
+	 * @var
+	 */
+	protected $response;
 
-    public function __construct()
-    {
-        parent::__construct();
+	public function __construct()
+	{
+		parent::__construct();
 
-        $this->client = new RestClient($this->getSecretToken(), $this->getAgreementGrantToken());
+		$this->client = new RestClient($this->getSecretToken(), $this->getAgreementGrantToken());
 
-        return $this;
-    }
+		return $this;
+	}
 
-    protected function getSecretToken()
-    {
-        return $this->config()->secretToken;
-    }
+	protected function getSecretToken()
+	{
+		return $this->config()->secretToken;
+	}
 
-    protected function getAgreementGrantToken()
-    {
-        return $this->config()->agreementGrantToken;
-    }
+	protected function getAgreementGrantToken()
+	{
+		return $this->config()->agreementGrantToken;
+	}
 
-    /**
-     * @return int
-     */
-    public function getCurrency()
-    {
-        return $this->config()->currency;
-    }
+	/**
+	 * @return int
+	 */
+	public function getCurrency()
+	{
+		return $this->config()->currency;
+	}
 
-    /**
-     * @return int
-     */
-    public function getInvoiceLayoutNumber()
-    {
-        return $this->config()->invoice_layout_number;
-    }
+	/**
+	 * @return int
+	 */
+	public function getInvoiceLayoutNumber()
+	{
+		return $this->config()->invoice_layout_number;
+	}
 
-    public function getPaymentTerms()
-    {
-        return $this->config()->payment_terms;
-    }
+	public function getPaymentTerms()
+	{
+		return $this->config()->payment_terms;
+	}
 
-    /**
-     * @return RestClient
-     */
-    public function getClient()
-    {
-        return $this->client;
-    }
+	/**
+	 * @return RestClient
+	 */
+	public function getClient()
+	{
+		return $this->client;
+	}
 
-    /**
-     * @return mixed
-     */
-    public function getResponse()
-    {
-        return $this->response;
-    }
+	/**
+	 * @return mixed
+	 */
+	public function getResponse()
+	{
+		return $this->response;
+	}
 
-    public function request()
-    {
-        return $this->getClient()->request;
-    }
+	public function request()
+	{
+		return $this->getClient()->request;
+	}
 
-    public function get($string, $params = null)
-    {
-        $this->response = $this->request()->get($string, $params);
+	public function get($string, $params = null)
+	{
+		$this->response = $this->request()->get($string, $params);
 
-        return $this->getResponse();
-    }
+		return $this->getResponse();
+	}
 
-    public function post($string, $params = null)
-    {
-        $this->response = $this->request()->post($string, $params);
+	public function post($string, $params = null)
+	{
+		$this->response = $this->request()->post($string, $params);
 
-        return $this->getResponse();
-    }
+		return $this->getResponse();
+	}
 
-    public function put($string, $params = null)
-    {
-        $this->response = $this->request()->put($string, $params);
+	public function put($string, $params = null)
+	{
+		$this->response = $this->request()->put($string, $params);
 
-        return $this->getResponse();
-    }
+		return $this->getResponse();
+	}
 
-    public function delete($string)
-    {
-        $this->response = $this->request()->delete($string);
+	public function delete($string)
+	{
+		$this->response = $this->request()->delete($string);
 
-        return $this->getResponse();
-    }
+		return $this->getResponse();
+	}
 
-    /**
-     * Shortcut for getting all customers
-     * @return Response
-     */
-    public function getCustomers($params = array('pagesize' => 400))
-    {
-        return $this->get('customers', $params);
-    }
+	/**
+	 * Shortcut for getting all customers
+	 * @return Response
+	 */
+	public function getCustomers($params = array('pagesize' => 400))
+	{
+		return $this->get('customers', $params);
+	}
 
-    /**
-     * Create customer in e-conomic
-     * @return Response
-     */
-    public function createCustomer($params)
-    {
-        return $this->post('customers', $params);
-    }
+	/**
+	 * Create customer in e-conomic
+	 * @return Response
+	 */
+	public function createCustomer($params)
+	{
+		return $this->post('customers', $params);
+	}
 
-    /**
-     * Update customer in e-conomic
-     * @return Response
-     */
-    public function updateCustomer($customerID, $params)
-    {
-        return $this->put('customers/' . $customerID, $params);
-    }
+	/**
+	 * Update customer in e-conomic
+	 * @return Response
+	 */
+	public function updateCustomer($customerID, $params)
+	{
+		return $this->put('customers/' . $customerID, $params);
+	}
 
-    /**
-     * Delete customer in e-conomic
-     * @return Response
-     */
-    public function deleteCustomer($customerID)
-    {
-        return $this->delete('customers/' . $customerID);
-    }
+	/**
+	 * Delete customer in e-conomic
+	 * @return Response
+	 */
+	public function deleteCustomer($customerID)
+	{
+		return $this->delete('customers/' . $customerID);
+	}
 
-    /**
-     * Create draft invoice
-     * @return Response
-     */
-    public function createInvoiceDraft($params)
-    {
-        return $this->post('invoices/drafts', $params);
-    }
+	/**
+	 * Create draft invoice
+	 * @return Response
+	 */
+	public function createInvoiceDraft($params)
+	{
+		return $this->post('invoices/drafts', $params);
+	}
 }
